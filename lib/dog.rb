65e9c0ef926ccc42fb957381b33090ff7b06d7ef
Sajid Anjum <sajid1760@gmail.com>
2020-03-13 21:46:53 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog 
+  
+  def name=(nameinput)
+    @name = nameinput
+  end
+  
+  def name
+    @name
+  end
+
+def breed=(breedinput)
+    @breed = breedinput
+  end
+  
+  def breed
+    @breed
+  end
+
+end
---
+++ lib/person.rb
@@ -0,0 +1,22 @@
+class Person
+  
+  def name=(nameinput)
+    @name = nameinput
+  end
+  
+  def name
+    @name
+  end
+
+def job=(jobinput)
+    @job = jobinput
+  end
+  
+  def job
+    @job
+  end
+
+
+
+
+end
